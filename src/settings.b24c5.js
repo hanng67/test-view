e520cd72a091160d6a0d997facc0d882dfcb271d
hanng67 <giahanng67@gmail.com>
2024-05-31 15:04:31 +0700

    - update

--- src/settings.b24c5.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","box","reward","group 3"],collisionMatrix:[[true],[false,false,true],[false,true,true],[false,false,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/jigsaw.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"69b9a",main:"1af69"}};
